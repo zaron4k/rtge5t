41ab0ebc94235ee84c93c727b414b5a309c809f8
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-09-22 11:23:32 +0300

    Add files via upload

---
+++ Олег и Даниил/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, созданный конструктором Windows Form Designer
+
+        private void InitializeComponent()
+        {
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(30, 31);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.PlaceholderText = "Введите a";
+            this.textBoxA.Size = new System.Drawing.Size(100, 23);
+            this.textBoxA.TabIndex = 0;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(30, 60);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.PlaceholderText = "Введите b";
+            this.textBoxB.Size = new System.Drawing.Size(100, 23);
+            this.textBoxB.TabIndex = 1;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(30, 90);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.PlaceholderText = "Введите c";
+            this.textBoxC.Size = new System.Drawing.Size(100, 23);
+            this.textBoxC.TabIndex = 2;
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(30, 120);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.PlaceholderText = "Введите x";
+            this.textBoxX.Size = new System.Drawing.Size(100, 23);
+            this.textBoxX.TabIndex = 3;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(30, 160);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(100, 23);
+            this.buttonCalculate.TabIndex = 4;
+            this.buttonCalculate.Text = "Вычислить";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(30, 200);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(66, 15);
+            this.labelResult.TabIndex = 5;
+            this.labelResult.Text = "F = (пусто)";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Name = "Form1";
+            this.Text = "Вариант 11";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
---
+++ Олег и Даниил/WinFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBoxA.Text);
+                double b = double.Parse(textBoxB.Text);
+                double c = double.Parse(textBoxC.Text);
+                double x = double.Parse(textBoxX.Text);
+
+                double F;
+
+                if (x < 1 && c != 0)
+                {
+                    F = a * Math.Pow(x, 2) + b / c;
+                }
+                else if (x > 1.5 && c == 0)
+                {
+                    F = (x - a) / Math.Pow((x - c), 2);
+                }
+                else
+                {
+                    F = Math.Pow(x, 2) / Math.Pow(c, 2);
+                }
+                labelResult.Text = $"F = {F}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка ввода данных: " + ex.Message);
+            }
+        }
+    }
+}
+
